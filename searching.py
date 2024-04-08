c6137703f8836f17795e21d23c973bbb30318d53
marekvecera <veceramarek02@gmail.com>
2024-04-08 11:43:57 +0200

    prvni commit

--- searching.py
+++ searching.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -12,10 +13,14 @@ def read_data(file_name, field):
     :return: (list, string),
     """
     file_path = os.path.join(cwd_path, file_name)
+    with open(file_path) as file:
+        data = json.load(file)
 
+    return data[field]
 
 def main():
-    pass
+    sequential_data = read_data("sequential.json", "unordered_numbers")
+    print(sequential_data)
 
 
 if __name__ == '__main__':
